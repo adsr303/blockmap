a1feb4bf8ae3ec71bb5e82b9031cf1ee812cc35e
Artur de Sousa Rocha <adsr@poczta.onet.pl>
2024-09-02 23:06:49 +0200

    First color+alpha implementation

--- convert/convert.go
+++ convert/convert.go
@@ -3,21 +3,50 @@ package convert
 import (
 	"image"
 	"strings"
+
+	"github.com/fatih/color"
 )
 
 func ConvertImageToTerminal(img image.Image) string {
 	var builder strings.Builder
 	for y := img.Bounds().Min.Y; y < img.Bounds().Max.Y; y++ {
 		for x := img.Bounds().Min.X; x < img.Bounds().Max.X; x++ {
-			r, g, b, _ := img.At(x, y).RGBA()
-			v := r/3 + g/3 + b/3
-			builder.WriteRune(gradient[v/alphaRange])
+			r, g, b, a := img.At(x, y).RGBA()
+			c := getColorIndex(r, g, b)
+			co := color.Set(colors[c]) // TODO Use Set/UnsetWriter
+			block := blocks[a/alphaRange]
+			co.Fprint(&builder, block, block)
 		}
 		builder.WriteRune('\n')
 	}
 	return builder.String()
 }
 
-var gradient = []rune{' ', '░', '▒', '▓', '█'}
+var blocks = []string{" ", "░", "▒", "▓", "█"}
+var colors = []color.Attribute{
+	color.FgBlack,
+	color.FgRed,
+	color.FgGreen,
+	color.FgYellow,
+	color.FgBlue,
+	color.FgMagenta,
+	color.FgCyan,
+	color.FgWhite,
+}
 
-const alphaRange = 0xffff / 5
+const alphaRange = 0xffff/5 + 1
+const halfBright = 0x7fff
+
+func getColorIndex(r, g, b uint32) int {
+	var result int
+	if r > halfBright {
+		result |= 0x1
+	}
+	if g > halfBright {
+		result |= 0x2
+	}
+	if b > halfBright {
+		result |= 0x4
+	}
+	return result
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,13 @@ module github.com/adsr303/blockmap
 
 go 1.23.0
 
-require golang.org/x/image v0.19.0
+require (
+	github.com/fatih/color v1.17.0
+	golang.org/x/image v0.19.0
+)
+
+require (
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	golang.org/x/sys v0.18.0 // indirect
+)
